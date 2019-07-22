0362508f9ca1ada22bc8bc3e58785179ef885554
Apple <apple@Apples-MacBook-Pro-8.local>
2019-07-22 15:20:16 -0400

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+// intetion: this playground test different comparisions between operators
+/*6 < 7
+18 < 10
+19 <= 68
+24 >= 43
+
+// test the == operator
+// we use 1 equals sign to set a value to something and we use a double equals to check
+12 == 10 + 2
+
+// test the not equal operator where the ! means negation
+12 != 10 + 2
+
+
+
+// check what && does ( AND)
+12 == 10 + 2 && 12 == 6 + 6 //true
+12 == 10 + 2 && 12 == 10 + 10 //false
+
+// check what || (OR)
+12 == 10 + 2 || 12 == 10 + 10 // true
+1 + 2 == 1 || 1 + 2 == 2
+
+5 < 3
+12 > 7
+6 != 8
+
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7 */
+
+// declaring an if statement  ( control, command, space ( for emojis))
+/*var dogAge = 12
+if dogAge < 2
+    {
+        print("You're are a puppy 🐶 ")
+    }
+else if dogAge < 6
+    {
+        print ("You are an average dog")
+    }
+else
+    {
+        print ("You are elderly")
+    } */
+
+var favfood = "Fried Shrimp"
+
+if favfood == "Chicken"
+    {
+        print("Hah you're a chicken")
+    }
+else if favfood == "Rice"
+    {
+        print("Mhmm love rice")
+    }
+else if favfood == "Fried Shrimp"
+    {
+        print("Love me some shrimp")
+    }
+else
+    {
+        print("Find a new food")
+    }
+
+
+
+
